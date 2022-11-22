2b0b317d0c5e996d37ba6458c87e47829e2ba1a0
Sinan <sinankzu@gmail.com>
2022-11-22 13:53:03 +0100

    aa

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBngi2yX1S__vnsF7FFDwb9BmGoxBJT_ko",
   authDomain: "fire-contact-app-427ea.firebaseapp.com",
-  projectId: "fire-contact-app-427ea",
   databaseURL:"https://fire-contact-app-427ea-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fire-contact-app-427ea",
   storageBucket: "fire-contact-app-427ea.appspot.com",
   messagingSenderId: "643787975026",
   appId: "1:643787975026:web:4dcfb87590743112bd55eb",
   measurementId: "G-ZME37791X0",
 };
 
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-
+export const auth = getAuth(firebase);
+export default firebase;
